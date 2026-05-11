9849dd62a40c2db1ee009aef4913d6b46481a438
SebastianOG509 <seortizg@unal.edu.co>
2026-05-11 12:00:52 -0500

    Mejorar el diseño del PDF de celulares: agregar encabezado, ajustar estilos y mejorar la presentación de la información.

--- resources/views/celular/pdf.blade.php
+++ resources/views/celular/pdf.blade.php
@@ -6,17 +6,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
     <link href="{{ public_path('css/app.css') }}" rel="stylesheet" type="text/css">
+
     <style>
+        .header {
+            width: 100%;
+            margin-bottom: 40px;
+        }
+
+        .header-table {
+            width: 100%;
+            border: none;
+        }
+
+        .header-table td {
+            border: none;
+            vertical-align: top;
+        }
+
         .logo {
-            float: left;
-            margin-right: 10px;
             width: 230px;
         }
 
+        .codigo-documento {
+            text-align: right;
+            font-size: 12px;
+            font-weight: bold;
+        }
+
         .fecha {
-            float: right;
-            margin-top: 0;
+            text-align: right;
+            font-size: 14px;
+            margin-top: 10px;
         }
 
         .cuerpo {
@@ -24,47 +46,88 @@
         }
 
         table {
-			border: 5px double gray; 
-			border-collapse: collapse;
-			font-size: 12px; 
-		}
-		td, th {
-			border: 2px solid gray; 
-			padding: 5px; 
-			text-align: center;
-		}
+            border: 5px double gray;
+            border-collapse: collapse;
+            font-size: 12px;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 2px solid gray;
+            padding: 5px;
+            text-align: center;
+        }
+
+        .sin-borde td {
+            border: none !important;
+        }
     </style>
 </head>
 
 <body class="cuerpo">
+
     <main>
-        <br>
-        <br>
-        <div>
-            <img src="{{ public_path('/img/logo_principal.png') }}" class="logo" alt="">
-            <h3 class="fecha">{{ $fechaActual }}</h3>
+
+        <!-- ENCABEZADO -->
+        <div class="header">
+
+            <table class="header-table sin-borde">
+                <tr>
+
+                    <!-- LOGO -->
+                    <td style="width: 60%; text-align: left;">
+                        <img src="{{ public_path('/img/logo_principal.png') }}" class="logo" alt="">
+                    </td>
+
+                    <!-- IDENTIFICADOR Y FECHA -->
+                    <td style="width: 40%; text-align: right;">
+
+                        <div class="codigo-documento">
+                            IT-INF-FR-003
+                        </div>
+
+                        <div class="fecha">
+                            {{ $fechaActual }}
+                        </div>
+
+                    </td>
+
+                </tr>
+            </table>
+
         </div>
-        <br><br><br><br><br><br>
+
         @foreach ($celulares as $celular)
+
             <p>Señor (a):</p>
+
             <h4>{{ $celular->empleado->nombre ?? 'El empleado no existe' }}</h4>
-            <p>Entrega de: </p>
+
+            <p>Entrega de:</p>
+
             <h4>{{ $celular->categoria->nombre ?? 'No existe' }}</h4>
+
             <p>Distinguido Señor (a):</p>
-            <p>Adjunto hacemos entrega de un(a) {{ $celular->categoria->nombre ?? 'No existe' }} con las
-                siguientes caracteristicas como parte de su dotación
-                laboral.</p>
+
+            <p>
+                Adjunto hacemos entrega de un(a)
+                {{ $celular->categoria->nombre ?? 'No existe' }}
+                con las siguientes características como parte de su dotación laboral.
+            </p>
+
             <table>
                 <tr>
                     <td>Marca</td>
                     <td>Modelo</td>
                     <td>N° Telefono</td>
-		    <td>Serial</td>
+                    <td>Serial</td>
                     <td>IMEI 1</td>
                     <td>IMEI 2</td>
                     <td>RAM</td>
                     <td>Memoria</td>
                 </tr>
+
                 <tr>
                     <td>{{ $celular->marca->marca ?? '' }}</td>
                     <td>{{ $celular->modelo ?? '' }}</td>
@@ -76,35 +139,68 @@
                     <td>{{ $celular->rom ?? '' }}</td>
                 </tr>
             </table>
+
             <br>
-            <p>Dicho celular puede ser utilizado de acuerdo a sus criterios en pro de los trabajos derivados de su
+
+            <p>
+                Dicho celular puede ser utilizado de acuerdo a sus criterios en pro de los trabajos derivados de su
                 actividad dentro de la compañía.
             </p>
-            <p>El usuario es responsable del manejo adecuado del equipo y de cualquier pérdida total o parcial del mismo. 
-		Si el equipo sufre daños debido a mal uso o negligencia que requieran reparación o reemplazo de piezas, en la primera incidencia 
-		el usuario asumirá el 50% del costo, mientras que la empresa cubrirá el 50% restante. En una segunda incidencia o posteriores, 
-		el usuario será responsable del 100% del costo. En caso de pérdida o robo, el usuario deberá cubrir el 100% del deducible 
-		correspondiente al seguro vigente del equipo, sin excepciones.
+
+            <p>
+                El usuario es responsable del manejo adecuado del equipo y de cualquier pérdida total o parcial del
+                mismo.
+                Si el equipo sufre daños debido a mal uso o negligencia que requieran reparación o reemplazo de piezas,
+                en la primera incidencia el usuario asumirá el 50% del costo, mientras que la empresa cubrirá el 50%
+                restante.
+                En una segunda incidencia o posteriores, el usuario será responsable del 100% del costo.
+                En caso de pérdida o robo, el usuario deberá cubrir el 100% del deducible correspondiente al seguro
+                vigente del equipo, sin excepciones.
             </p>
+
             <br>
+
             <p>Agradezco su amable y acostumbrada atención</p>
+
             <p>Atentamente.</p>
+
             <br>
+
             <div style="display: flex; align-items: center;">
+
                 <div style="float: left;">
-                    <p style="font-size: 16px;"> <b>FREDDY JAVIER ALONSO </b><br>
-                    ADMINISTRATIVE DIRECTOR</p>
-                    <p style="font-size: 13px;">FIRMA _____________________________ <br><br>
-                        C.C ________________________________</p>
+                    <p style="font-size: 16px;">
+                        <b>FREDDY JAVIER ALONSO</b><br>
+                        ADMINISTRATIVE DIRECTOR
+                    </p>
+
+                    <p style="font-size: 13px;">
+                        FIRMA _____________________________
+                        <br><br>
+                        C.C ________________________________
+                    </p>
                 </div>
+
                 <div style="float:right; text-align: left;">
-                    <p style="font-size: 14px;"> <b>{{$celular->empleado->nombre ?? '' }}</b> <br>
-                        {{ $celular->empleado->cargos->nombre ?? ''}}</p>
-                    <p style="font-size: 13px;">FIRMA __________________________________ <br><br>
-                    C.C _____________________________________</p>
+                    <p style="font-size: 14px;">
+                        <b>{{ $celular->empleado->nombre ?? '' }}</b>
+                        <br>
+                        {{ $celular->empleado->cargos->nombre ?? '' }}
+                    </p>
+
+                    <p style="font-size: 13px;">
+                        FIRMA __________________________________
+                        <br><br>
+                        C.C _____________________________________
+                    </p>
                 </div>
+
             </div>
+
         @endforeach
+
     </main>
+
 </body>
+
 </html>
